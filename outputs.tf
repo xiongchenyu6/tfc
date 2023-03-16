5777d0dc099987b7787f03ae6df53b2399327ecc
freeman <xiongchenyu6@gmail.com>
2023-03-16 12:50:20 +0800

    Add: Improve data processing and visualization in project.
    - Refactor and simplify codebase for improved readability and maintainability
    - Implement new feature for user authentication and authorization
    - Optimize queries for improved performance and database efficiency

--- outputs.tf
+++ outputs.tf
@@ -9,12 +9,10 @@ output "ssh_public_key" {
   value = data.digitalocean_ssh_key.office.public_key
 }
 
-
-
 output "ssh_public_fingerprint" {
   value = data.digitalocean_ssh_key.office.fingerprint
 }
 
-
-
-
+output "ip" {
+  value = digitalocean_droplet.digital-1.ipv4_address
+}
