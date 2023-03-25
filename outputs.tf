0c34f5d4862111ab99dfaf53547e12cd03b8eb86
freeman <xiongchenyu6@gmail.com>
2023-03-25 16:45:01 +0800

    Add:

--- outputs.tf
+++ outputs.tf
@@ -5,14 +5,14 @@
 #   value = aws_instance.ubuntu.ami
 # }
 
-output "ssh_public_key" {
-  value = data.digitalocean_ssh_key.office.public_key
-}
+# output "ssh_public_key" {
+#   value = data.digitalocean_ssh_key.office.public_key
+# }
 
-output "ssh_public_fingerprint" {
-  value = data.digitalocean_ssh_key.office.fingerprint
-}
+# output "ssh_public_fingerprint" {
+#   value = data.digitalocean_ssh_key.office.fingerprint
+# }
 
-output "ip" {
-  value = digitalocean_droplet.digital-1.ipv4_address
-}
+# output "ip" {
+#   value = digitalocean_droplet.digital-1.ipv4_address
+# }
