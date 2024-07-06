7105945cdc65057e6a1935204f8764c172c907bb
xiongchenyu <xiongchenyu6@gmail.com>
2024-07-06 21:32:05 +0800

    update ip

--- main.tf
+++ main.tf
@@ -36,7 +36,8 @@ resource "cloudflare_record" "vr_autolife" {
 resource "cloudflare_record" "authentik_autolife" {
   zone_id = cloudflare_zone.autolife.id
   name    = "authentik"
-  value   = "159.65.8.132"
+  value   = "206.189.156.130"
   type    = "A"
+  proxied = false
   ttl     = 3600
 }
